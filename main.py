c823dc383c59adbf2d6cafc9683d37fa0d3e6b71
unknown <sergey55.chz@gmail.com>
2021-12-29 10:39:46 +0300

    code updates

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 import codecs
+import json
 
 
-import yaml, json
+import yaml
 import numpy as np
 from scipy import spatial
 
-from utils import html_to_text
+from utils import url_to_text
 from vectorizer import TextVectorizer
 
 
@@ -17,20 +18,23 @@ if __name__ == '__main__':
     with codecs.open('config.yml', encoding='utf-8') as f:
         d_config = yaml.load(f, Loader=yaml.FullLoader)
 
+    n_closest = int(d_config['n_closest'])
+
+
     text_vectorizer = TextVectorizer(language=d_config['language'],
                                      min_sent_len=d_config['min_sent_len'])
 
+    with codecs.open(d_config['url_list'], 'r', encoding='utf-8') as f:
+        urls = [line.rstrip() for line in f]
 
     output = []
-    for f_name in os.listdir(d_config['data_path']):
+    for url in urls:
         d_out = {}
-        f_name_full = os.path.join(d_config['data_path'], f_name)
-        text = html_to_text(f_html=f_name_full)
+        text = url_to_text(url)
         response, v = text_vectorizer.vectorize(text)
         if v is None:
             continue
-        #vs.append(v)
-        d_out['title'] = f_name
+        d_out['title'] = url
         d_out['v'] = v.tolist()
         output.append(d_out)
 
@@ -41,13 +45,12 @@ if __name__ == '__main__':
     for n_item, item in enumerate(output):
         pos_closest = np.argsort(dists[n_item])[1:]
         titles_closest = [output[pos]['title'] for pos in pos_closest]
-        item['titles_closest'] = titles_closest
-        item['titles_closest_ws'] = [1 - dists[n_item][pos] for pos in pos_closest]
+        item['titles_closest'] = titles_closest[:n_closest]
+        #item['titles_closest_ws'] = [1 - dists[n_item][pos] for pos in pos_closest]
         del item['v']
 
 
-
-    with open(os.path.join(d_config['data_path'], 'result.txt'), 'w', encoding='utf8') as f:
+    with open('result.txt', 'w', encoding='utf8') as f:
         json.dump(output, f, ensure_ascii=False, indent=4)
 
 
--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
-from bs4 import BeautifulSoup
 import codecs
+import requests
 
-def html_to_text(f_html):
+from bs4 import BeautifulSoup
 
-    with codecs.open(f_html, encoding='utf-8') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-    text = soup.find_all(text=True)
-    res = ''
+
+def url_to_text(url):
+    text = ''
+
+    try:
+        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+    except:
+        return text
+
     blacklist = [
         '[document]',
         'noscript',
@@ -19,8 +24,7 @@ def html_to_text(f_html):
         'dl'
         # there may be more elements you don't want, such as "style", etc.
     ]
-    for t in text:
+    for t in soup.find_all(text=True):
         if t.parent.name not in blacklist:
-            res += '{} '.format(t)
-    return res
-
+            text += '{} '.format(t)
+    return text
